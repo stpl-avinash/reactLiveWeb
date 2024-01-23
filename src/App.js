2e080578678145d7e5b0d68c72ecef2a25a8b657
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2024-01-23 17:50:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Header from "./Component/Header";
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
+      <Home />
       {/* <Header /> */}
     <Routes>
       {/* <Route  path="/" element={ <Header/> } /> */}
